04365e020b3d7c4915571c6fd93b1f87d42a591c
Hamid Darvishi <151385998+Minimillius@users.noreply.github.com>
2023-12-31 16:50:25 +0000

    Update MultiClassSVMclassify.m

--- MI/MultiClassSVMclassify.m
+++ MI/MultiClassSVMclassify.m
@@ -1,27 +1,10 @@
-function [output] = MultiClassSVMclassify(mdl,datatest)
+function output = MultiClassSVMclassify(mdl, datatest)
+    numClasses = numel(mdl);
 
-for i = 1:size(datatest,2)
-    x = datatest(:,i);
-    y = svmclassify(mdl.svm1,x');
-    if y == 1
-        output(i) = 1;
-    elseif y ==2 
-        y = svmclassify(mdl.svm2,x');
-        if y == 1
-            output(i) =2 ;
-        elseif y == 2
-            y = svmclassify(mdl.svm3,x');
-            if y == 1
-                output(i) = 3;
-            elseif y == 2
-                y = svmclassify(mdl.svm4,x');
-                if y == 1
-                    output(i) =4 ;
-                elseif y == 2
-                    output(i) = nan;
-                end
-            end
-        end
+    output = nan(1, size(datatest, 2));
+
+    for i = 1:numClasses
+        classIndices = svmclassify(mdl(i).svm, datatest');
+        output(classIndices == 1) = i;
     end
-end
 end
