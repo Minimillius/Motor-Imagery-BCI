99bd9cc299bf14952aa8bcc887c90e3a164d965e
Hamid Darvishi <151385998+Minimillius@users.noreply.github.com>
2023-12-31 16:55:44 +0000

    Update myMulticClassCSP1.m

--- MI/myMulticClassCSP1.m
+++ MI/myMulticClassCSP1.m
@@ -1,25 +1,8 @@
-function [w] = myMulticClassCSP1(datatrain1,datatrain2,datatrain3,datatrain4,m)
+function w = myMulticClassCSP1(datatrain1, datatrain2, datatrain3, datatrain4, m)
+    w1 = myCSP(datatrain1, cat(3, datatrain2, datatrain3, datatrain4), m);
+    w2 = myCSP(datatrain2, cat(3, datatrain1, datatrain3, datatrain4), m);
+    w3 = myCSP(datatrain3, cat(3, datatrain1, datatrain2, datatrain4), m);
+    w4 = myCSP(datatrain4, cat(3, datatrain1, datatrain2, datatrain3), m);
 
-%% 1 versus 2 3 4
-data1 = datatrain1;
-data2 = cat(3,datatrain2,datatrain3,datatrain4);
-[w1]  = myCSP(data1,data2,m);
-
-%% 2 versus 1 3 4
-data1 = datatrain2;
-data2 = cat(3,datatrain1,datatrain3,datatrain4);
-[w2]  = myCSP(data1,data2,m);
-
-%% 3 versus 1 2 4
-data1 = datatrain3;
-data2 = cat(3,datatrain1,datatrain2,datatrain4);
-[w3]  = myCSP(data1,data2,m);
-
-%% 4 versus 1 2 3
-data1 = datatrain4;
-data2 = cat(3,datatrain1,datatrain2,datatrain3);
-[w4]  = myCSP(data1,data2,m);
-
-w = cat(3,w1,w2,w3,w4);
-
+    w = cat(3, w1, w2, w3, w4);
 end
