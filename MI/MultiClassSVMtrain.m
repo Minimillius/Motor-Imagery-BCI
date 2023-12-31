9b2504f29353bce80b2d7d48f36ca154ef3a877c
Hamid Darvishi <151385998+Minimillius@users.noreply.github.com>
2023-12-31 16:51:35 +0000

    Update MultiClassSVMtrain.m

--- MI/MultiClassSVMtrain.m
+++ MI/MultiClassSVMtrain.m
@@ -1,32 +1,13 @@
-function [mdl] = MultiClassSVMtrain(featuretrain1,featuretrain2,featuretrain3,featuretrain4)
+function mdl = MultiClassSVMtrain(featuretrain1, featuretrain2, featuretrain3, featuretrain4)
+    numClasses = 4;
+    mdl = struct('svm', cell(1, numClasses));
 
-%% 1 versus 2 3 4
-data1 = featuretrain1;
-data2 = cat(2,featuretrain2,featuretrain3,featuretrain4);
+    for i = 1:numClasses
+        data1 = eval(['featuretrain', num2str(i)]);
+        data2 = cat(2, eval(['featuretrain', num2str(setdiff(1:numClasses, i))]));
 
-datatrain  = [data1,data2];
-labeltrain = [ones(1,size(data1,2)),2*ones(1,size(data2,2))];
-mdl.svm1   = svmtrain(datatrain',labeltrain);
-
-%% 2 versus 1 3 4
-data1 = featuretrain2;
-data2 = cat(2,featuretrain1,featuretrain3,featuretrain4);
-datatrain  = [data1,data2];
-labeltrain = [ones(1,size(data1,2)),2*ones(1,size(data2,2))];
-mdl.svm2   = svmtrain(datatrain',labeltrain);
-
-%% 3 versus 1 2 4
-data1 = featuretrain3;
-data2 = cat(2,featuretrain1,featuretrain2,featuretrain4);
-datatrain  =[data1,data2];
-labeltrain = [ones(1,size(data1,2)),2*ones(1,size(data2,2))];
-mdl.svm3   = svmtrain(datatrain',labeltrain);
-
-%% 4 versus 1 2 3
-data1 = featuretrain4;
-data2 = cat(2,featuretrain1,featuretrain2,featuretrain3);
-datatrain  = [data1,data2];
-labeltrain = [ones(1,size(data1,2)),2*ones(1,size(data2,2))];
-mdl.svm4   = svmtrain(datatrain',labeltrain);
-
+        datatrain  = [data1, data2];
+        labeltrain = [ones(1, size(data1, 2)), 2 * ones(1, size(data2, 2))];
+        mdl(i).svm = svmtrain(datatrain', labeltrain);
+    end
 end
